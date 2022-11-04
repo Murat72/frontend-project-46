fbbccdb09c97c4155f102964213caa811ac0c24a
murat72 <muratkunanbaev1991@gmail.com>
2022-11-04 12:37:48 +0500

    update test

--- __test__/gendiff.test.js
+++ __test__/gendiff.test.js
@@ -1,23 +1,23 @@
 import { fileURLToPath } from 'url';
 import path from 'path';
+import fs from 'fs';
 import gendiff from '../src/index.js';
 
 const __dirname = path.dirname(fileURLToPath(import.meta.url));
 
 const getFixturePath = (filename) => path.join(__dirname, '..', '__fixtures__', filename);
+const readFile = (filename) => fs.readFileSync(getFixturePath(filename), 'utf-8');
+
+const expectedStylish = readFile('stylish.txt');
 
 test('genDiffJSON', () => {
   const firstFile = getFixturePath('file1.json');
   const secondFile = getFixturePath('file2.json');
-  const expected = '{\n - follow: false\n   host: hexlet.io\n - proxy: 123.234.53.22\n - timeout: 50\n + timeout: 20\n + verbose: true\n}';
-  const actual = gendiff(firstFile, secondFile);
-  expect(actual).toEqual(expected);
+  expect(gendiff(firstFile, secondFile)).toEqual(expectedStylish);
 });
 
 test('genDiffYAML', () => {
   const firstFile = getFixturePath('file1.yaml');
   const secondFile = getFixturePath('file2.yml');
-  const expected = '{\n - follow: false\n   host: hexlet.io\n - proxy: 123.234.53.22\n - timeout: 50\n + timeout: 20\n + verbose: true\n}';
-  const actual = gendiff(firstFile, secondFile);
-  expect(actual).toEqual(expected);
+  expect(gendiff(firstFile, secondFile)).toEqual(expectedStylish);
 });
