71e610fca4506b9ea7565565a53a45c1017aa499
murat72 <muratkunanbaev1991@gmail.com>
2022-11-04 21:21:20 +0500

    fixed bug

--- src/stylish.js
+++ src/stylish.js
@@ -38,7 +38,7 @@ const stylish = (tree) => {
           return `${getIndent(depth)}- ${key}: ${stringify(value1, depth + 1)}\n${getIndent(depth)}+ ${key}: ${stringify(value2, depth + 1)}`;
         }
         case 'unchanged': {
-          return `${getIndent(depth)}  ${key}: ${stringify(value, depth + 1)}`;
+          return `${getIndent(depth)} ${key}: ${stringify(value, depth + 1)}`;
         }
         default:
           throw new Error(`This type does not exist: ${node.type}`);
