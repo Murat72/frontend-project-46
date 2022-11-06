21524d2bd4b6c05abfc043fdfb15faf78e278b49
murat72 <muratkunanbaev1991@gmail.com>
2022-11-06 21:17:38 +0500

    move to separate file function buildDiff

---
+++ src/buildDiff.js
@@ -0,0 +1,25 @@
+import _ from 'lodash'
+
+const buildDiff = (data1, data2) => {
+  const keys = _.union(Object.keys(data1), Object.keys(data2));
+  const sortedKeys = _.sortBy(keys);
+  return sortedKeys.map((key) => {
+    if (!_.has(data1, key)) {
+      return { type: 'added', key, value: data2[key] };
+    }
+    if (!_.has(data2, key)) {
+      return { type: 'deleted', key, value: data1[key] };
+    }
+    if (_.isObject(data1[key]) && _.isObject(data2[key])) {
+      return { type: 'inserted', key, children: buildDiff(data1[key], data2[key]) };
+    }
+    if (data1[key] !== data2[key]) {
+      return {
+        type: 'changed', key, value1: data1[key], value2: data2[key],
+      };
+    }
+    return { type: 'unchanged', key, value: data1[key] };
+  });
+};
+
+export default buildDiff;
