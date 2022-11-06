295665ff3ceb1a01ecb3e153b115503cef719f6f
murat72 <muratkunanbaev1991@gmail.com>
2022-11-06 21:14:11 +0500

    add test work plain formatter

--- __test__/gendiff.test.js
+++ __test__/gendiff.test.js
@@ -8,7 +8,8 @@ const __dirname = path.dirname(fileURLToPath(import.meta.url));
 const getFixturePath = (filename) => path.join(__dirname, '..', '__fixtures__', filename);
 const readFile = (filename) => fs.readFileSync(getFixturePath(filename), 'utf-8');
 
-const expectedStylish = readFile('stylish.txt');
+const expectedStylish = readFile('expectedStylish.txt');
+const expectedPlain = readFile('expectedPlain.txt');
 
 test('genDiffJSON', () => {
   const firstFile = getFixturePath('file1.json');
@@ -20,4 +21,16 @@ test('genDiffYAML', () => {
   const firstFile = getFixturePath('file1.yaml');
   const secondFile = getFixturePath('file2.yml');
   expect(gendiff(firstFile, secondFile)).toBe(expectedStylish);
+});
+
+test('plainJSON', () => {
+  const firstFile = getFixturePath('file1.json');
+  const secondFile = getFixturePath('file2.json');
+  expect(gendiff(firstFile, secondFile, 'plain')).toBe(expectedPlain);
+});
+
+test('plainYaml', () => {
+  const firstFile = getFixturePath('file1.yaml');
+  const secondFile = getFixturePath('file2.yml');
+  expect(gendiff(firstFile, secondFile, 'plain')).toBe(expectedPlain);
 });
